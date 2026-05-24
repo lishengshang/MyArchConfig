a096f07c2ab1df674ca12307341a7f39c488f50c
lishengshang <3490017805@qq.com>
2026-05-24 17:41:13 +0800

    changed zsh and fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,34 +10,3 @@ alias dotfiles='git --git-dir=$HOME/dotfiles --work-tree=$HOME'
 starship init fish | source
 zoxide init fish --cmd cd | source
 
-function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
-end
-
-function cat
-	if command -q bat
-		command bat $argv
-	else
-		command cat $argv
-	end
-end
-
-function ls
-	if command -q eza
-		command eza --icons $argv
-	else
-		command ls $argv
-	end
-end
-function lt
-	if command -q eza
-		command eza --icons --tree $argv
-	else
-		command ls --tree $argv 2>/dev/null || command ls -R $argv
-	end
-end
