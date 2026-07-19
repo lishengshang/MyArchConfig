fcd8a35c68c91afb0caf8694d6561271c63b20d1
lishengshang <3490017805@qq.com>
2026-07-19 23:22:10 +0800

    cleanup: remove backup-fish-config.fish (replaced by dot stash/tag)

--- .config/fish/functions/backup-fish-config.fish
+++
@@ -1,5 +0,0 @@
-function backup-fish-config -d "Backup current fish configuration"
-    set -l backup_dir "$HOME/.config/fish.backup."(date +%Y%m%d_%H%M%S)
-    cp -r ~/.config/fish $backup_dir
-    echo "Fish 配置已备份到: $backup_dir"
-end
