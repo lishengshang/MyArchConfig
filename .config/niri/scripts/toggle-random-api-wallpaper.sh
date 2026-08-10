59dac50e6b8c7b835203bf15e3c3845cd34e64b8
lishengshang <3490017805@qq.com>
2026-08-10 21:17:01 +0800

    auto: 2026-08-10 21:17:01
    改动文件 (1): .config/niri/scripts/toggle-random-api-wallpaper.sh
    - 新增/未跟踪: 0
    - 修改: 1
    - 删除: 0
    
    (由 systemd timer dotfiles-autocommit.timer 自动提交)

--- .config/niri/scripts/toggle-random-api-wallpaper.sh
+++ .config/niri/scripts/toggle-random-api-wallpaper.sh
@@ -10,9 +10,9 @@ notify() {
 
 if systemctl --user is-active --quiet "$TIMER"; then
     systemctl --user disable --now "$TIMER"
-    notify "自动壁纸已关闭" "每 5 分钟随机切换已停止"
+    notify "自动壁纸已关闭" "每 8 分钟随机切换已停止"
 else
     systemctl --user enable --now "$TIMER"
     systemctl --user start random-api-wallpaper.service
-    notify "自动壁纸已开启" "每 5 分钟随机切换已启动"
+    notify "自动壁纸已开启" "每 8 分钟随机切换已启动"
 fi
